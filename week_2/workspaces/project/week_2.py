be8222c073b15dc224c3e148e5491272437bdc0a
David Goussev <dgoussev@gmail.com>
2022-12-04 00:46:05 +0000

    week_2 project commit

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -4,30 +4,41 @@ from dagster import In, Nothing, Out, ResourceDefinition, String, graph, op
 from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
+@op(
+    config_schema={'s3_key': String},
+    required_resource_keys={"s3"}
+    )
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return list(csv_helper(context.op_config["s3_key"]))
 
-@op
-def get_s3_data():
-    pass
+@op(    
+    ins = {"stocks": In(dagster_type=List[Stock], description="s3 list stock data")},
+    out = {"high_aggregation": Out(dagster_type=Aggregation, description="highest stock price/date aggregation")}
+)
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    high_stock = max(stocks, key=lambda stock:stock.high)
+    return Aggregation(date=high_stock.date, high=high_stock.high)
 
+@op (
+    required_resource_keys={"redis"}
+)
+def put_redis_data(context, high_aggregation: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=str(high_aggregation.date), value=str(high_aggregation.high))
 
-@op
-def process_data():
-    pass
-
-
-@op
-def put_redis_data():
-    pass
-
-
-@op
-def put_s3_data():
-    pass
+@op (
+    required_resource_keys={"s3"}
+)
+def put_s3_data(context, high_aggregation: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key=high_aggregation.date, data=high_aggregation)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    stocks = get_s3_data()
+    processed = process_data(stocks)
+    put_redis_data(processed)
+    put_s3_data(processed)
 
 
 local = {
@@ -56,8 +67,18 @@ docker = {
 
 week_2_pipeline_local = week_2_pipeline.to_job(
     name="week_2_pipeline_local",
+    config= local,
+    resource_defs = {
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource()
+    }
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config= docker,
+    resource_defs = {
+        "s3": s3_resource,
+        "redis": redis_resource
+    }
 )
