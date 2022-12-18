9a9fe4db76fda7d8e91db63b4bf20aa197c36462
David Goussev <dgoussev@gmail.com>
2022-12-18 20:48:47 +0000

    week_4 project commit

--- week_4/workspaces/project/week_4.py
+++ week_4/workspaces/project/week_4.py
@@ -5,28 +5,60 @@ from workspaces.resources import redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@asset
-def get_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    group_name="corise"
+)
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return [Stock.from_list(item) for item in context.resources.s3.get_data(s3_key)]
 
 
-@asset
-def process_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    group_name="corise"
+)
+def process_data(stocks: List[Stock]) -> Aggregation:
+    high_stock = max(stocks, key=lambda stock:stock.high)
+    return Aggregation(date=high_stock.date, high=high_stock.high)
 
 
-@asset
-def put_redis_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    required_resource_keys={"redis"},
+    group_name="corise"
+)
+def put_redis_data(context, high_aggregation: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=str(high_aggregation.date), value=str(high_aggregation.high))
 
 
-@asset
-def put_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    required_resource_keys={"s3"},
+    group_name="corise"
+)
+def put_s3_data(context, high_aggregation: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key_name=str(high_aggregation.date), data=high_aggregation)
 
 
-get_s3_data_docker, process_data_docker, put_redis_data_docker, put_s3_data_docker = with_resources()
+get_s3_data_docker, process_data_docker, put_redis_data_docker, put_s3_data_docker = with_resources(
+    definitions=[get_s3_data, process_data, put_redis_data, put_s3_data],
+    resource_defs={
+        "s3": s3_resource, 
+        "redis": redis_resource
+    },
+    resource_config_by_key={
+        "redis": {
+            "config": {
+                "host": "redis",
+                "port": 6739,
+            }
+        },
+        "s3": {
+            "config": {
+                "bucket": "dagster",
+                "access_key": "test",
+                "secret_key": "test",
+                "endpoint_url": "http://localstack:4566",
+            }
+        },
+    },
+)
